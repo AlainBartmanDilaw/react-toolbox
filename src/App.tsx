94dd656fe3e51b24193273a9d1c930676615c5fe
Alain LEGLISE <alainleglise@outlook.fr>
2023-07-22 20:51:43 +0200

    Add Carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,44 @@
-import { ConfigProvider } from 'antd';
+import { Carousel, ConfigProvider } from 'antd';
 import React from 'react';
 import 'src/App.css';
+import { version } from 'src/autobuild_version';
+import images from 'src/images';
 import ShowContrats from 'src/ShowContrats';
 import ShowMatch from 'src/ShowMatch';
-import {version} from 'src/autobuild_version';
 
 
 const App: React.FC = () => {
 
     return (
 
-    <ConfigProvider
-        theme={{
-            token: {
-                colorPrimary: '#0b557e',
-            },
-        }}
-    >
-        <ShowMatch/>
-        <ShowContrats/>
+        <ConfigProvider
+            theme={{
+                token: {
+                    colorPrimary: '#0b557e',
+                },
+            }}
+        >
+            <div className="carousel-container">
+                <Carousel autoplay autoplaySpeed={3000}>
+                    {
+                        images.map(
+                            (image: string, index: number) => (
+                                <div key={index}>
+                                    <img src={image} alt={`${index + 1}`}/>
+                                </div>
+                            )
+                        )
+                    }
+                </Carousel>
+            </div>
+            <ShowMatch/>
+            <ShowContrats/>
 
-        <footer>
-            Version courante : {version}
-        </footer>
+            <footer>
+                Version courante : {version}
+            </footer>
 
-    </ConfigProvider>
+        </ConfigProvider>
 
     );
 
@@ -32,3 +46,4 @@ const App: React.FC = () => {
 
 
 export default App;
+
---
+++ src/images.tsx
@@ -0,0 +1,33 @@
+import img00 from "./assets/images/1150540.jpg" ;
+import img01 from "./assets/images/1150542.jpg" ;
+import img02 from "./assets/images/1150554.jpg" ;
+import img03 from "./assets/images/1150561.jpg" ;
+import img04 from "./assets/images/1150702.jpg" ;
+import img05 from "./assets/images/1150705.jpg" ;
+import img06 from "./assets/images/1150738.jpg" ;
+import img07 from "./assets/images/1150797.jpg" ;
+import img08 from "./assets/images/1150799.jpg" ;
+import img09 from "./assets/images/1150802.jpg" ;
+import img10 from "./assets/images/1150804.jpg" ;
+import img11 from "./assets/images/1150812.jpg" ;
+import img12 from "./assets/images/563319.jpg" ;
+import img13 from "./assets/images/1150517.jpg" ;
+
+const images = [
+    img00,
+    img01,
+    img02,
+    img03,
+    img04,
+    img05,
+    img06,
+    img07,
+    img08,
+    img09,
+    img10,
+    img11,
+    img12,
+    img13,
+];
+
+export default images;
